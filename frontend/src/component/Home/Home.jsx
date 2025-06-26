72f7362d5b2caf6c918f27209ebb97aefe6976d5
anonymous-lad2 <anonymouslad540@gmail.com>
2025-06-26 15:42:40 +0530

    restaurant card

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import { MultiItemCarousel } from './MultiItemCarousel'
+import RestaurantCard from '../Restaurant/RestaurantCard';
 
+const restaurant = [1,1,1,1,1,1,1,1];
 export const Home = () => {
   return (
-    <div>
+    <div className='pb-10'>
         <section className='banner -z-50 relative flex flex-col justify-center items-center'>
 
             <div className='w-[50vw] z-10 text-center'>
@@ -24,6 +26,18 @@ export const Home = () => {
         <section className='p-10 lg:py-10 lg:px-20'>
             <p className='text-2xl font-semibold text-gray-400 py-3 pb-10'>Top Meals</p>
             <MultiItemCarousel/>
+        </section>
+
+        <section className='px-5 lg:px-20 pt-10'> 
+            <h1 className='text-2xl font-semibold text-gray-400 pb-8'>Orders From Our Handpicked Favourites</h1>
+
+            <div className='flex flex-wrap items-center justify-around gap-5'>
+                {
+                    restaurant.map((item) => (
+                        <RestaurantCard/>
+                    ))
+                }
+            </div>
         </section>
     </div>
   )
