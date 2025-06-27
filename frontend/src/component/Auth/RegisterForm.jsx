14a55ce2d47c53fd39d41a6a25e7725d408c8ac6
anonymous-lad2 <anonymouslad540@gmail.com>
2025-06-27 18:18:28 +0530

    register form

--- frontend/src/component/Auth/RegisterForm.jsx
+++ frontend/src/component/Auth/RegisterForm.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import { Typography, TextField, Button, Select } from "@mui/material";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  role: "",
+};
 
 const RegisterForm = () => {
-  return (
-    <div>RegisterForm</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default RegisterForm
+  const handleSubmit = (values) => {
+    console.log("form values: ",values)
+  };
+  return (
+    <div>
+      <Typography variant="h5" className="text-center">
+        Register
+      </Typography>
+
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <Field
+            as={TextField}
+            name="fullName"
+            label="Full Name"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+          <Field
+            as={TextField}
+            name="email"
+            label="Email"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+
+          <Field
+            as={TextField}
+            name="password"
+            label="Password"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            type="password"
+          />
+
+            <FormControl fullWidth>
+                <InputLabel id="role-simple-select-label">Role</InputLabel>
+            <Field
+                fullWidth
+                margin="normal"
+                as={Select}
+              labelId="role-simple-select-label"
+              id="demo-simple-select"
+              name="role"
+            //   value="role"
+              label="Role"
+            //   onChange={handleChange}
+            >
+              <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+              <MenuItem value={"ROLE_RESTAURANT_OWNER"}>Restaurant Owner</MenuItem>
+            </Field>
+            </FormControl>
+
+          <Button
+            sx={{ mt: 2, padding: "1rem" }}
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Register
+          </Button>
+        </Form>
+      </Formik>
+
+      <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+        If have an account already?
+        <Button size="small" onClick={() => navigate("/account/login")}>
+          Login
+        </Button>
+      </Typography>
+    </div>
+  );
+};
+
+export default RegisterForm;
