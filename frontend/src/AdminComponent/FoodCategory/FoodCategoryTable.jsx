0d708d7904648af04ead4ead4de6c4924a6abebf
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-25 01:08:42 +0530

    food category modal created

--- frontend/src/AdminComponent/FoodCategory/FoodCategoryTable.jsx
+++ frontend/src/AdminComponent/FoodCategory/FoodCategoryTable.jsx
@@ -8,17 +8,32 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import CreateIcon from '@mui/icons-material/Create';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Modal from '@mui/material/Modal';
+import CreateFoodCategoryForm from "./CreateFoodCategoryForm";
 
 const orders = [1,1,1,1,1,1]
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const FoodCategoryTable = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Box>
       <Card className="mt-1">
         <CardHeader title={"Food Categories"} sx={{ pt: 2, alignItems: "center" }} 
             action={
-            <IconButton>
+            <IconButton onClick={handleOpen}>
                 <CreateIcon/>
             </IconButton>
             } 
@@ -45,6 +60,17 @@ const FoodCategoryTable = () => {
           </Table>
         </TableContainer>
       </Card>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <CreateFoodCategoryForm/>
+        </Box>
+      </Modal>
     </Box>
   );
 };
