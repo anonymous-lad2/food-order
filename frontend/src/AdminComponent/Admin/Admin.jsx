d4f55e461a5624cfb0b5e7a6c9e19090721d387f
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-24 17:47:30 +0530

    folder structure

--- frontend/src/AdminComponent/Admin/Admin.jsx
+++ frontend/src/AdminComponent/Admin/Admin.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import AdminSideBar from './AdminSideBar'
+import { Route, Routes } from 'react-router-dom'
+import Orders from '../Orders/Orders'
+import Menu from '../Menu/Menu'
+import FoodCategory from '../FoodCategory/FoodCategory'
+import Ingredients from '../Ingredients/Ingredients'
+import Events from '../Events/Events'
+import RestaurantDetails from './RestaurantDetails'
+import RestaurantDashboard from '../Dashboard/Dashboard'
 
 const Admin = () => {
     const handleClose = () => {
@@ -12,7 +20,16 @@ const Admin = () => {
                 <AdminSideBar handleClose={handleClose}/>
             </div>
             <div className='lg:w-[80%]'>
+                <Routes>
+                    <Route path='/' element={<RestaurantDashboard/>}/>
+                    <Route path='/orders' element={<Orders/>}/>
+                    <Route path='/menu' element={<Menu/>}/>
+                    <Route path='/category' element={<FoodCategory/>}/>
+                    <Route path='/ingredients' element={<Ingredients/>}/>
+                    <Route path='/event' element={<Events/>}/>
+                    <Route path='/details' element={<RestaurantDetails/>}/>
 
+                </Routes>
             </div>
         </div>
     </div>
