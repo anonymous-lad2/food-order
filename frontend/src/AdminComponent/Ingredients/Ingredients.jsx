c645493d81506eb7c7b0bf799595a3f2d7417f3c
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-24 20:19:11 +0530

    ingredient page

--- frontend/src/AdminComponent/Ingredients/Ingredients.jsx
+++ frontend/src/AdminComponent/Ingredients/Ingredients.jsx
@@ -1,8 +1,20 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import IngredientTable from './IngredientTable'
+import IngredientCategoryTable from './IngredientCategoryTable'
 
 const Ingredients = () => {
   return (
-    <div>Ingredients</div>
+    <div className='px-2'>
+        <Grid container spacing={2} >
+            <Grid item size={{ xs: 12, lg: 8 }}>
+                <IngredientTable/>
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 4 }}>
+                <IngredientCategoryTable/>
+            </Grid>
+        </Grid>
+    </div>
   )
 }
 
